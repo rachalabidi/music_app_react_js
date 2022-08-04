4b20305e573fc9fe5da9f79d80b054c2da42f38b
rachalabidi <rashalabidi@gmail.com>
2022-08-05 00:36:28 +0200

    making a background color to the active song

--- music_app/src/components/LibrarySong.js
+++ music_app/src/components/LibrarySong.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LibrarySong = ({song ,songs , setCurrentSong ,  audioRef ,isPlaying})=>{
+const LibrarySong = ({song ,songs , setCurrentSong , id, audioRef ,isPlaying,setSongs})=>{
     const songSelectHandler =()=>{
 setCurrentSong(song)
 if(isPlaying){
@@ -11,10 +11,24 @@ if(isPlaying){
     })
    }
 }
+const newSongs=  songs.map((song)=>{
+    if(song.id === id){
+        return{
+            ...song,
+            active: true,
+        }
+    }else{
+        return{
+        ...song,
+        active: false,
+        }
+    } 
+   })
+   setSongs(newSongs)
 
     }
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
         <img src={song.cover} alt="cover"></img>
         <div  className="song-desc">
         <h2>{song.name}</h2>
